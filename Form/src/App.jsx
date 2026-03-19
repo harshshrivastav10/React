c2710b11704a0287decf2228266b627bbadd251e
Harsh Shrivastav <harshshrivastav1220@gmail.com>
2026-03-19 15:34:38 +0000

    form

---
+++ Form/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import MyForm from './Component/Main_page'
+
+function App() {
+
+  return (
+    <>
+    <MyForm/>
+    </>
+  )
+}
+
+export default App
